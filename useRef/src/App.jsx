483dd3079731a0fa0d64f39a1d44e5377f47ffec
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-02-25 14:13:24 +0000

    useRef

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -12,20 +12,45 @@ import './App.css'
 function App() {
 
   const ref = useRef(0)
-  const inputRef = useRef(null)
+  const inputRef1 = useRef(null)
+  const inputRef2 = useRef(null)
+  const inputRef3 = useRef(null)
   useEffect(() => {
     console.log('Component Rendered');
   })
-  function handleClick () {
+  function handleClick1 () {
     // ref.current = ref.current + 1
     // console.log(ref.current);
-    inputRef.current.focus()
-    inputRef.current.style.backgroundColor = "aqua"
+    inputRef1.current.focus()
+    inputRef1.current.style.backgroundColor = "aqua"
+    inputRef2.current.style.backgroundColor = ""
+    inputRef3.current.style.backgroundColor = ""
+  }
+  function handleClick2 () {
+    // ref.current = ref.current + 1
+    // console.log(ref.current);
+    inputRef2.current.focus()
+    inputRef2.current.style.backgroundColor = "aqua"
+    inputRef1.current.style.backgroundColor = ""
+    inputRef3.current.style.backgroundColor = ""
+  }
+  function handleClick3 () {
+    // ref.current = ref.current + 1
+    // console.log(ref.current);
+    inputRef3.current.focus()
+    inputRef3.current.style.backgroundColor = "aqua"
+    inputRef1.current.style.backgroundColor = ""
+    inputRef2.current.style.backgroundColor = ""
   }
   return (
     <>
-    <button onClick={handleClick}>Click me</button> <br />
-    <input ref={inputRef} type="text" />
+    <button onClick={handleClick1}>Click me 1</button> <br />
+    <input ref={inputRef1} type="text" /> <br />
+    <button onClick={handleClick2}>Click me 2</button> <br />
+    <input ref={inputRef2} type="text" />
+    <br />
+    <button onClick={handleClick3}>Click me 3</button> <br />
+    <input ref={inputRef3} type="text" />
     </>
   )
 }
