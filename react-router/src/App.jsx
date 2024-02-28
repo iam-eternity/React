02136a895fe2f5b6997f8f2848ced1378c3caef6
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-02-28 15:27:18 +0000

    ch

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import Header from '../components/header/header'
+import Header from './components/header/header'
 import './App.css'
-import Footer from '../components/footer/footer'
+import Footer from './components/footer/footer'
+import Home from './components/home/home'
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   )
