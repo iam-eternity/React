75847221fbc86c255d1867cd823286d28a0de00b
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-02-29 08:32:50 +0000

    ch

--- react-router/src/Layout.jsx
+++ react-router/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/header/header'
-import Footer from './components/footer/footer'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 import { Outlet } from 'react-router-dom'
 export default function Layout() {
     return (
