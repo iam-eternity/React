829c8dba0df0bb0f937c2493fd11a70a0223e73b
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-02-29 09:22:57 +0000

    ch

---
+++ react-router/src/components/Github/Github.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Github() {
+    return (
+        <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">Github Repositries</div>
+    )
+}
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './App.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 
 // const router = createBrowserRouter([
@@ -30,8 +33,10 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
-        <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} />
     </Route>
   )
 )
