e24b864e8f2105b40b92c317fa70d8b9a12291b5
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-02-29 10:21:54 +0000

    done

--- react-router/src/components/Github/Github.jsx
+++ react-router/src/components/Github/Github.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 export default function Github() {
+    const data = useLoaderData()
+    // const [ data, setData ] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/iam-eternity')
+    //     .then(response => response.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    //  }, [])
+
     return (
-        <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">Github Repositries</div>
-    )
+        <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">Github Followers : {data.followers} 
+        <img src={data.avatar_url} alt="Git Picture" width={300} />
+        </div>
+        )
+}
+export const infoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/iam-eternity')
+    return response.json()
 }
