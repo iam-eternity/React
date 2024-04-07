e80953d7e553ee561ffed07589c3fce10cbe71d6
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-04-07 11:00:17 +0000

    ch

---
+++ 09useContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,19 @@
+const React = require("react");
+
+import UserContext from './UserContext'
+const userContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return (
+    <UserContext.Provider value= {{user, setUser}}>
+        {children}
+    </UserContext.Provider>
+    )
+}
+
+
+export default userContextProvider
+
+
+
+
+
